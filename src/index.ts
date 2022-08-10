1507113a0b8999120778a6fe0d154a037712a4aa
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-09 23:38:27 -0300

    feat: Requisito 13 - campo de batalha - criado 1 objeto do tipo PVP

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
+import { PVP } from './Battle';
 import Character from './Character';
 import Dragon from './Dragon';
 import Monster from './Monster';
 
+// Players--------------------------------------
 const player1 = new Character('Lakimn');
 const player2 = new Character('Nightmare');
 const player3 = new Character('Noob');
 
 Array(12).forEach(() => player1.levelUp());
+// ----------------------------------------------
 
+// Monsters--------------------------------------
 const monster1 = new Monster();
 const monster2 = new Dragon();
+// ----------------------------------------------
+
+// Battle-----------------------------------------
+const pvp = new PVP(player2, player3);
 
 export { 
   player1,
@@ -17,4 +25,5 @@ export {
   player3,
   monster1,
   monster2,
+  pvp,
 };
