2c798351b480d0905d2214f6a6e9c9068bb9802f
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-09 21:01:19 -0300

    feat: Requisito 10 - criada a class PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,20 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(private player1: Fighter, private player2: Fighter) {
+    super(player1);
+  }
+
+  fight(): number {
+    for (; this.player1.lifePoints > 0 && this.player2.lifePoints > 0;) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+
+    if (this.player1.lifePoints > 0) return 1;
+    return -1;
+  }
+}
+
+export default PVP;
