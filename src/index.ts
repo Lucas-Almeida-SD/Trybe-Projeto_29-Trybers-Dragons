31479b8c50b59e9b8cab8a067d899376f4c81c7a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-08-09 23:35:13 -0300

    feat: Requisito 13 - campo de batalha - criados 1 objeto do tipo Monster e 1 do tipo Dragon

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
 
 const player1 = new Character('Lakimn');
 const player2 = new Character('Nightmare');
@@ -6,8 +8,13 @@ const player3 = new Character('Noob');
 
 Array(12).forEach(() => player1.levelUp());
 
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
 export { 
   player1,
   player2,
   player3,
+  monster1,
+  monster2,
 };
